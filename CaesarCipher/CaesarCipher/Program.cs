8870a57a4fa764cd930cde78be28f90efa759770
Joanarfc <joanarfcarvalho@hotmail.com>
2022-05-06 21:11:24 +0100

    Removed the alphabet from the Main

--- CaesarCipher/CaesarCipher/Program.cs
+++ CaesarCipher/CaesarCipher/Program.cs
@@ -10,7 +10,6 @@ namespace CaesarCipher
     {
         static void Main(string[] args)
         {
-            char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
             string secretMessage = EncryptMessage(InputMessageToChar(), 3);
             Console.WriteLine(secretMessage);
