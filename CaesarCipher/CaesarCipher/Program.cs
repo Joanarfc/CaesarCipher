26af3518cf8f5d220f6c55311b3e85681750fb94
Joanarfc <joanarfcarvalho@hotmail.com>
2022-05-06 21:10:47 +0100

    Added the methods InputMessageToChar() and EncryptMessage()

--- CaesarCipher/CaesarCipher/Program.cs
+++ CaesarCipher/CaesarCipher/Program.cs
@@ -12,44 +12,50 @@ namespace CaesarCipher
         {
             char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
+            string secretMessage = EncryptMessage(InputMessageToChar(), 3);
+            Console.WriteLine(secretMessage);
+
+        }
+
+        static char[] InputMessageToChar()
+        {
             string message;
 
             Console.WriteLine("Please enter the secret message: ");
             message = Console.ReadLine().ToLower();
-
-            char[] secretMessage = message.ToCharArray();
-
-            char[] encryptedMessage = new char[secretMessage.Length];
             
-            for(int i = 0; i < secretMessage.Length; i++)
+
+            return message.ToCharArray();
+        }
+
+        static string EncryptMessage(char[] message, int key)
+        {
+            char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            char[] encryptedMessage = new char[message.Length];
+
+            for (int i = 0; i < message.Length; i++)
             {
-                char charSecretMessage = secretMessage[i];
+                char charSecretMessage = message[i];
 
-
-                //Since the Array.IndexOf return -1 when it doesn't find an object in the 'alphabet' array, I used the if-else to verify if the character is found in the 'alphabet' array or not.
-                //If it's a symbol, it is not in the array and so it returns -1. If this is the case, then we simply display the message[i], that is, the original symbol inserted in the message.
-                //If it's not a symbol, the Array.IndexOf will find the character in the array, and so it will encrypt the character.
-                if(Array.IndexOf(alphabet, charSecretMessage) != -1)
+                if (Array.IndexOf(alphabet, charSecretMessage) != -1)
                 {
                     //find the letter position in the array alphabet.
                     //Note: we need to use the modulo because of the cases in which we have a 'z' letter, otherwise, it will expand the limit of the alphabet when we add the 3 positions to the right(it would be 28 letters).
-                    //This way, we make sure that the letter position will never go past 26
-                    int letterPosition = (Array.IndexOf(alphabet, charSecretMessage) + 3) % 26;
+                    //This way, we make sure that the letter position will never go further than 26
+                    int letterPosition = (Array.IndexOf(alphabet, charSecretMessage) + key) % alphabet.Length;
                     encryptedMessage[i] = alphabet[letterPosition];
 
                 }
                 else
                 {
-                    encryptedMessage[i] = secretMessage[i];
+                    encryptedMessage[i] = message[i];
+
                 }
 
-
-                
-
             }
-
             string finalMessage = String.Join("", encryptedMessage);
-            Console.WriteLine(finalMessage);
+            return finalMessage;
 
         }
     }
