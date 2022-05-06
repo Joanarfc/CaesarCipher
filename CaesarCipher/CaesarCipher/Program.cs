454c425aaec214fc7a4f9c2aec98d198369e91a0
Joanarfc <joanarfcarvalho@hotmail.com>
2022-05-06 20:18:15 +0100

    Added logic to handle the insertion of symbols in the message

--- CaesarCipher/CaesarCipher/Program.cs
+++ CaesarCipher/CaesarCipher/Program.cs
@@ -25,13 +25,24 @@ namespace CaesarCipher
             {
                 char charSecretMessage = secretMessage[i];
 
-                //find the letter position in the array alphabet.
-                //Note: we need to use the modulo because of the cases in which we have a 'z' letter, otherwise, it will expand the limit of the alphabet when we add the 3 positions to the right(it would be 28 letters).
-                //This way, we make sure that the letter position will never go past 26
-                int letterPosition = (Array.IndexOf(alphabet, charSecretMessage) + 3) % 26;
-                char encryptedChar = alphabet[letterPosition];
 
-                encryptedMessage[i] = encryptedChar;
+                //Since the Array.IndexOf return -1 when it doesn't find an object in the 'alphabet' array, I used the if-else to verify if the character is found in the 'alphabet' array or not.
+                //If it's a symbol, it is not in the array and so it returns -1. If this is the case, then we simply display the message[i], that is, the original symbol inserted in the message.
+                //If it's not a symbol, the Array.IndexOf will find the character in the array, and so it will encrypt the character.
+                if(Array.IndexOf(alphabet, charSecretMessage) != -1)
+                {
+                    //find the letter position in the array alphabet.
+                    //Note: we need to use the modulo because of the cases in which we have a 'z' letter, otherwise, it will expand the limit of the alphabet when we add the 3 positions to the right(it would be 28 letters).
+                    //This way, we make sure that the letter position will never go past 26
+                    int letterPosition = (Array.IndexOf(alphabet, charSecretMessage) + 3) % 26;
+                    encryptedMessage[i] = alphabet[letterPosition];
+
+                }
+                else
+                {
+                    encryptedMessage[i] = secretMessage[i];
+                }
+
 
                 
 
